380ca2dfc91a3a4c8f3d709a60805dd9b7a71289
Alexander Nesterenko <nestorf250@gmail.com>
2015-12-02 11:43:50 +0200

    Released normal display scanning  - Added listeners for click and item count change into Adapter  - Made background of entry in scanning list clickable visually  - Updated libraries  - Added .gitignore

--- app/src/main/java/io/github/cleac/bluetoothletest/ui/adapter/BluetoothListAdapter.java
+++ app/src/main/java/io/github/cleac/bluetoothletest/ui/adapter/BluetoothListAdapter.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import butterknife.ButterKnife;
 import io.github.cleac.bluetoothletest.R;
 import io.github.cleac.bluetoothletest.utils.BluetoothScanner;
 
@@ -23,19 +24,30 @@ import io.github.cleac.bluetoothletest.utils.BluetoothScanner;
 public class BluetoothListAdapter extends RecyclerView.Adapter<BluetoothListAdapter.ViewHolder> {
     public static final String LOG_TAG = BluetoothListAdapter.class.getSimpleName();
 
+    public interface OnItemClickedListener {
+        void onItemClicked(BluetoothDevice device);
+    }
+
+    public interface OnItemAddedListener {
+        void onItemAddedListener(int newCount);
+    }
+
     private List<BluetoothDevice> mDevices;
     private String mFilter;
-    private BluetoothScanner bluetoothScanner;
+    private BluetoothScanner mBluetoothScanner;
+    private OnItemClickedListener mOnItemClickedListener;
+    private OnItemAddedListener mOnItemAddedListener;
+
 
     public BluetoothScanner getBluetoothScanner() {
-        return bluetoothScanner;
+        return mBluetoothScanner;
     }
 
     public BluetoothListAdapter(Context context) {
-        bluetoothScanner = new BluetoothScanner(context);
-        bluetoothScanner.setLeScanCallback((device, rssi, scanRecord) -> appendDevice(device));
-        bluetoothScanner.setOnScanningRestart(this::clearDevices);
-        bluetoothScanner.setOnScanningStart(this::clearDevices);
+        mBluetoothScanner = new BluetoothScanner(context);
+        mBluetoothScanner.setLeScanCallback((device, rssi, scanRecord) -> appendDevice(device));
+        mBluetoothScanner.setOnScanningRestart(this::clearDevices);
+        mBluetoothScanner.setOnScanningStart(this::clearDevices);
         mDevices = new ArrayList<>();
         mFilter = null;
     }
@@ -48,7 +60,7 @@ public class BluetoothListAdapter extends RecyclerView.Adapter<BluetoothListAdap
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.entry_bt_scan_list,parent,false);
+                .inflate(R.layout.entry_bt_scan_list, parent, false);
         return new ViewHolder(view);
     }
 
@@ -57,15 +69,21 @@ public class BluetoothListAdapter extends RecyclerView.Adapter<BluetoothListAdap
         final BluetoothDevice bluetoothDevice = mDevices.get(position);
         holder.name.setText(bluetoothDevice.getName());
         holder.address.setText(bluetoothDevice.getAddress());
+        holder.rootLayout.setOnClickListener((v) -> {
+            if (mOnItemClickedListener != null)
+                mOnItemClickedListener.onItemClicked(mDevices.get(position));
+        });
     }
 
     public void appendDevice(BluetoothDevice device) {
-        Log.d(LOG_TAG, "Found device " + device.getName());
-        if(!mDevices.contains(device)
-                || mFilter == null
-                || Pattern.matches(mFilter, device.getName())) {
+        if (!mDevices.contains(device)
+                && (mFilter == null
+                || Pattern.matches(mFilter, device.getName()))) {
+            Log.d(LOG_TAG, "Found device " + device.getName());
+            notifyItemInserted(mDevices.size());
             mDevices.add(device);
-            notifyDataSetChanged();
+            if (mOnItemAddedListener != null)
+                mOnItemAddedListener.onItemAddedListener(mDevices.size());
         }
     }
 
@@ -92,12 +110,24 @@ public class BluetoothListAdapter extends RecyclerView.Adapter<BluetoothListAdap
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView name;
         TextView address;
+        View rootLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
-            name = (TextView) itemView.findViewById(R.id.name);
-            address = (TextView) itemView.findViewById(R.id.address);
+            name = ButterKnife.findById(itemView, R.id.name);
+            address = ButterKnife.findById(itemView, R.id.address);
+            rootLayout = ButterKnife.findById(itemView, R.id.root_layout);
+            rootLayout.setClickable(true);
         }
     }
+
+    public void setOnItemClickedListener(OnItemClickedListener onItemClickedListener) {
+        mOnItemClickedListener = onItemClickedListener;
+    }
+
+    public void setOnItemAddedListener(OnItemAddedListener onItemAddedListener) {
+        mOnItemAddedListener = onItemAddedListener;
+    }
+
 }
--- app/src/main/java/io/github/cleac/bluetoothletest/ui/fragment/BluetoothScanFragment.java
+++ app/src/main/java/io/github/cleac/bluetoothletest/ui/fragment/BluetoothScanFragment.java
@@ -8,9 +8,12 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,7 +31,10 @@ public class BluetoothScanFragment extends Fragment {
 
     public static final String LOG_TAG = BluetoothScanFragment.class.getSimpleName();
 
-    @Bind(R.id.list) RecyclerView recyclerView;
+    @Bind(R.id.list)
+    RecyclerView mDevicesRecyclerView;
+    @Bind(R.id.layout_scanning)
+    View mScanningLayout;
     BluetoothListAdapter bluetoothListAdapter;
 
     public static final int ENABLE_BLUETOOTH = 666;
@@ -37,36 +43,40 @@ public class BluetoothScanFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_scan_bluetooth,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_scan_bluetooth, container, false);
         if (!getActivity().getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE) ||
                 !getActivity().getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
-            Toast.makeText(getContext(), "BLE is not supported by device", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), "BLE is not supported by device", Toast.LENGTH_SHORT).show();
             getActivity().finish();
         }
-        ButterKnife.bind(this,rootView);
-        bluetoothListAdapter = new BluetoothListAdapter(getContext())
+        ButterKnife.bind(this, rootView);
+        bluetoothListAdapter = new BluetoothListAdapter(getActivity())
                 .setNameFilter(null);
-        if (Build.VERSION.SDK_INT >= 23 &&
-                getContext().checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                    GET_LOCATION_PERMISSION);
-        }
+        bluetoothListAdapter.setOnItemClickedListener((device) -> Toast.makeText(getActivity(),
+                "Clicked device " + device.getAddress(), Toast.LENGTH_SHORT).show());
+        bluetoothListAdapter.setOnItemAddedListener((newCount -> mScanningLayout.setVisibility
+                ((newCount == 0) ? View.VISIBLE : View.GONE)));
+        mDevicesRecyclerView.setAdapter(bluetoothListAdapter);
+        mDevicesRecyclerView.setLayoutManager(new
+
+                LinearLayoutManager(getActivity()
+
+        ));
         return rootView;
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case GET_LOCATION_PERMISSION:
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
                     bluetoothListAdapter.getBluetoothScanner().startScanning(false);
-                } else {
-                    Snackbar.make(getView().findViewById(R.id.root_bt_scan),
+                else
+                    Snackbar.make(
+                            getView().findViewById(R.id.root_bt_scan),
                             "Cannot get scanning results from bluetooth adapter",
                             Snackbar.LENGTH_LONG
                     ).show();
-                }
         }
     }
 
@@ -74,19 +84,22 @@ public class BluetoothScanFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        if(bluetoothListAdapter.getBluetoothScanner()!=null) {
-            if (!bluetoothListAdapter.getBluetoothScanner().isBluetoothAvailable()) {
-                startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), ENABLE_BLUETOOTH);
-            } else {
-                bluetoothListAdapter.getBluetoothScanner().startScanning(false);
-            }
-        }
+        if (Build.VERSION.SDK_INT >= 23 &&
+                getContext().checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED)
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    GET_LOCATION_PERMISSION);
+        if (!bluetoothListAdapter.getBluetoothScanner().isBluetoothAvailable())
+            startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), ENABLE_BLUETOOTH);
+        else
+            bluetoothListAdapter.getBluetoothScanner().startScanning(false);
+
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        if(bluetoothListAdapter.getBluetoothScanner()!=null)
+        if (bluetoothListAdapter.getBluetoothScanner() != null)
             bluetoothListAdapter.getBluetoothScanner().stopScanning();
     }
 
@@ -94,7 +107,7 @@ public class BluetoothScanFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
             case ENABLE_BLUETOOTH:
-                if(resultCode == Activity.RESULT_OK) {
+                if (resultCode == Activity.RESULT_OK) {
                     bluetoothListAdapter.getBluetoothScanner().startScanning(false);
                 } else {
                     Snackbar.make(
